b4aebd2a360159e2dc5c3c3aaba5320e56e71f7f
nidan2110 <nidangavali1@gmail.com>
2024-10-05 01:52:06 +0530

    Added docstring for tests

--- ui_automation/tests/test_homepage.py
+++ ui_automation/tests/test_homepage.py
@@ -4,18 +4,30 @@ from pages.home_page import HomePage
 @pytest.mark.usefixtures("setup")
 class TestHomePage:
     def test_search_product(self):
+        """
+        Test to search product from searchbar
+        """
         home_page = HomePage(self.driver)
         home_page.search_product("iPhone")
         assert "Search - iPhone" in self.driver.title
 
     def test_featured_products_displayed(self):
+        """
+        Test to check if the product are displayed
+        """
         home_page = HomePage(self.driver)
         assert home_page.is_featured_product_displayed()
 
     def test_logo_is_displayed(self):
+        """
+        Test to verify the logo is displayed
+        """
         home_page = HomePage(self.driver)
         assert home_page.image_is_displayed()
 
     def test_cart_btn_is_clicked(self):
+        """
+        Test to validate the working of cart button
+        """
         home_page = HomePage(self.driver)
         assert home_page.cart_btn_working()
